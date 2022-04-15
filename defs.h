c0e0dc91b28763f3d042d1d6ab631ccf794d594f
Antony Merle <antony.merle@gmail.com>
2022-04-15 22:41:10 +0200

    enum type KEY_TYPE

--- defs.h
+++ defs.h
@@ -4,3 +4,8 @@
 #define READ_FILE_BUFFER_16K0		1024*16			// buffer to hash file
 #define HEX_SHA256_NULLT_LEN		65				// 64 + 1
 #define MAX_SIG_LENGTH				256
+
+typedef enum {
+	PKEY,		// public key
+	SKEY		// secret key
+} KEY_TYPE;
