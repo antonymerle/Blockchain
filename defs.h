15ff41845e9bbf65c6012c406eeaaca0d6c563cf
Antony Merle <antony.merle@gmail.com>
2022-04-18 16:07:09 +0200

    new enum IO_BUFFER_SZ replace defines

--- defs.h
+++ defs.h
@@ -1,11 +1,14 @@
 #pragma once
 #include "common.h"
-
-#define READ_FILE_BUFFER_16K0		1024*16			// buffer to hash file
-#define HEX_SHA256_NULLT_LEN		65				// 64 + 1
-#define MAX_SIG_LENGTH				256
-
-typedef enum {
+	
+typedef enum KEY_TYPE {
 	PKEY,		// public key
 	SKEY		// secret key
 } KEY_TYPE;
+
+typedef enum IO_BUFFER_SZ {
+	READ_FILE_BUFFER_16K0 = 1024 * 16,									// buffer to hash file
+	HEX_HASH_NT = 65,												// SHA256 hexadecimal 64 + 1
+	SIG_BIN = 256,													// SHA256 value calculated by openSSL EVP_DigestSignFinal()
+	SIG_HEX_NT = (SIG_BIN * 2) + 1
+} IO_BUFFER_SZ;
