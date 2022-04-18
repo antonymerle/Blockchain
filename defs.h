4d19c91785de04b339392eb5b70943d7b89e1731
Antony Merle <antony.merle@gmail.com>
2022-04-18 18:25:41 +0200

    align

--- defs.h
+++ defs.h
@@ -8,7 +8,7 @@ typedef enum KEY_TYPE {
 
 typedef enum IO_BUFFER_SZ {
 	READ_FILE_BUFFER_16K0 = 1024 * 16,									// buffer to hash file
-	HEX_HASH_NT = 65,												// SHA256 hexadecimal 64 + 1
-	SIG_BIN = 256,													// SHA256 value calculated by openSSL EVP_DigestSignFinal()
+	HEX_HASH_NT = 65,													// SHA256 hexadecimal 64 + 1
+	SIG_BIN = 256,														// SHA256 value calculated by openSSL EVP_DigestSignFinal()
 	SIG_HEX_NT = (SIG_BIN * 2) + 1
 } IO_BUFFER_SZ;
